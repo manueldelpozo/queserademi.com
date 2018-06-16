14ccb1e842365ed17c8639134854bce2ad60aae8
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2018-06-17 00:45:30 +0200

    feat: Implement header component and service for dynamic and sanitized title

--- qsdm-app/src/app/app.module.ts
+++ qsdm-app/src/app/app.module.ts
@@ -4,20 +4,19 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { QsdmHeaderComponent } from './qsdm-header/qsdm-header.component';
 import { QsdmFooterComponent } from './qsdm-footer/qsdm-footer.component';
 import { QsdmMainpageComponent } from './qsdm-mainpage/qsdm-mainpage.component';
 import { QsdmQuienesSomosComponent } from './qsdm-quienes-somos/qsdm-quienes-somos.component';
 import { QsdmColaboraComponent } from './qsdm-colabora/qsdm-colabora.component';
+
 import { ROUTING } from './app.routing';
-
-
 import { QsdmServiceService } from './qsdm-service.service';
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
+    QsdmHeaderComponent,
     QsdmFooterComponent,
     QsdmQuienesSomosComponent,
     QsdmColaboraComponent,
